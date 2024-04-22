47e400600425be50e2d1599886e81459105ccce4
umidjonisamuxamediv <umidjonisamuxamedov@gmail.com>
2024-04-22 13:40:41 +0500

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig}*/
+const nextConfig = {
+    unoptimized:true,
+   images:{
+    domains:
+    [
+        'm.media-amazon.com',"lh3.googleusercontent.com"
+    ],
+   },
+   
+};
+
+export default nextConfig
